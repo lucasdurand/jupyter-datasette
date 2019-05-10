68e572dc7868513c3c621f2b4d3493964c64b83b
Durand <Lucas.Durand@tdsecurities.com>
2019-05-10 16:44:46 -0400

    Refactor into __init__ and tools

--- jupyter_datasette/__init__.py
+++ jupyter_datasette/__init__.py
@@ -1,49 +1,73 @@
-import os, sys
-import json
-from notebook.utils import url_path_join
+from notebook.utils import _url_path_join
 from notebook.base.handlers import IPythonHandler
-import socket
-import re
-import subprocess
+import sys as _sys
+import os as _os
+import socket as _socket
+from IPython.display import display as _display
+from IPython.display import HTML as _HTML
 
 # TODO: real config class for this
-path = os.environ.get('DATASETTE_HOME',os.path.expanduser('~/Datasette'))
-datasette_host = os.environ.get('DATASETTE_HOST',None) # provide a static hostname to keep users happy
+datasette_path = _os.environ.get('DATASETTE_HOME',_os.path.expanduser('~/Datasette'))
+datasette_host = _os.environ.get('DATASETTE_HOST',None) # provide a static hostname to keep users happy
 
 from . import tools
 
-class DatasetteHolder():
+# Pandas DataFrame to Datasette
+def pandas_to_datasette(df, name='this', table_name='table', db_root='./', if_exists='replace', logging=True, **kwargs):
+    # create the DB from csv
+    db_name = tools.pandas_to_sqlite(df, name, table_name, db_root, if_exists) 
+    # now to launch it ...
+    return Datasette(db_name, jupyter=True, logging=logging, **kwargs)
+    
+# TODO: register Jupyter Magic
+
+class Datasette():
     '''
     I hold and manage a Datasette server. I pick up changes to existing databases, 
-        but need to be reloaded to pick up new fils
+        but need to be reloaded to pick up new files
     '''
-    def __init__(self, home):
-        self.path=home
-        self.launch()
+    def __init__(self, files=[], folder=None, logging=False, jupyter=True, **kwargs):
+        self.files =  files
+        self.path = folder
+        self.jupyter = jupyter
+        self._launch()
 
     def reload(self):
+        '''Terminate existing Datasette process and start on another available port'''
+
+        # TODO: can we run this from a service that detects newly added files?
         self.kill()
-        self.launch()
+        self._launch()
         return
 
-    def launch(self):
-        # TODO: clean this up to use the launcher from `tools`
-        files = ' '.join([os.path.join(self.path,x) for x in os.listdir(self.path) if x[-3:]=='.db'])
+    def _launch(self):
+        '''Only to be called directly in __init__ for first launch'''
+        if self.path:
+            files = ' '.join([_os.path.join(self.path,x) for x in _os.listdir(self.path) if x[-3:]=='.db'])
+        else:
+            files = ' '.join(self.files) if type(self.files) is list else self.files
         self.port = tools.find_free_port()
-        cmd = f'datasette {files} --reload --port {self.port} --host 0.0.0.0'
-        cmd = re.sub(' +', ' ', cmd)
-        # TODO: can we run this from a service that detects newly added files?
+
         # TODO: use pexpect so we're sure that it's `Goin' Fast` before we try and load the page
-        self.process = subprocess.Popen(cmd.split(' '))
+        self.process = tools.start_datasette(files, jupyter=self.jupyter, port=self.port, host='0.0.0.0', reload=True)
         self.pid = self.process.pid
-        message = f"Launching Datasette at {self.path}"
-        return message
+        
+        if self.logging: # this is blocking
+            try:
+                for line in iter(self.process.stderr.readline, b''):
+                    _sys.stdout.write(line)
+            except: # KeyboardInterrupt or some other calamity
+                self.process.terminate()     
 
     def kill(self):
-        self.process.kill()
-        os.waitpid(self.pid,0)
+        self.process.terminate()
+        self.process.wait()
+        return
 
-class DatasetteHandler(IPythonHandler):
+    def __del__(self):
+        self.kill()
+
+class _DatasetteHandler(IPythonHandler):
     def get(self):
         """
         Return location of running Datasette
@@ -54,13 +78,12 @@ class DatasetteHandler(IPythonHandler):
             datasette.reload()
         else:
             port = datasette.port
-            host = datasette_host if datasette_host else socket.gethostbyname(socket.gethostname())
+            host = datasette_host if datasette_host else _socket.gethostbyname(_socket.gethostname())
             self.redirect(f'http://{host}:{port}')
-            #self.write(json.dumps({'port':port,'localurl':f'http://localhost:{port}'}))
 
 def _jupyter_server_extension_paths():
     """
-    Set up the server extension for collecting metrics
+    Set up the server extension for managing Datasette
     """
     return [{
         'module': 'jupyter-datasette',
@@ -68,7 +91,7 @@ def _jupyter_server_extension_paths():
 
 def _jupyter_nbextension_paths():
     """
-    Set up the notebook extension for displaying metrics
+    Set up the notebook extension for displaying Datasette
     """
     return [{
         "section": "tree",
@@ -79,9 +102,9 @@ def _jupyter_nbextension_paths():
 
 def load_jupyter_server_extension(nbapp):
     """
-    Called during notebook start
+    Called during jupyter server start
     """
-    datasette = DatasetteHolder(path)
+    datasette = Datasette(datasette_path, jupyter=False)
     nbapp.web_app.datasette = datasette
-    route_pattern = url_path_join(nbapp.web_app.settings['base_url'], '/datasette')
-    nbapp.web_app.add_handlers('.*', [(route_pattern, DatasetteHandler)])
+    route_pattern = _url_path_join(nbapp.web_app.settings['base_url'], '/datasette')
+    nbapp.web_app.add_handlers('.*', [(route_pattern, _DatasetteHandler)])
--- jupyter_datasette/tools.py
+++ jupyter_datasette/tools.py
@@ -5,6 +5,7 @@ import os as _os
 import socket as _socket
 from IPython.display import display as _display
 from IPython.display import HTML as _HTML
+import re
 
 import sqlite3 as _sqlite3
 from contextlib import closing as _closing
@@ -31,7 +32,7 @@ def pandas_to_sqlite(df, name='this', table_name='table', db_root='./', if_exist
     df.to_sql(table_name, conn, if_exists=if_exists)
     return db_name
 
-def start_datasette(db_path, show_errors=True, **kwargs):
+def start_datasette(db_path, jupyter, **kwargs):
     '''
         Pass in cmdline flags as kwargs: port=8080, reload=True
     '''
@@ -42,29 +43,13 @@ def start_datasette(db_path, show_errors=True, **kwargs):
     flags = [f'--{key}' if value is True else f'--{key} {value}' for key,value in kwargs.items()]
     flags = ' '.join(flags)
     cmd = f'datasette {db_path} {flags}'
-    _display(_HTML(f'''<b>Datasette Launching at <a target="_blank" href="http://{host}:{port}">http://{host}:{port}</a></b>'''))
+    cmd = re.sub(' +', ' ', cmd)
+
+    if jupyter:
+        _display(_HTML(f'''<b>Datasette Launching at <a target="_blank" href="http://{host}:{port}">http://{host}:{port}</a></b><br/>
+            <pre>{cmd}</pre>'''))
     try:
         process = _subprocess.Popen(cmd.split(' '), stderr=_subprocess.PIPE, stdout=_subprocess.PIPE)
-        if show_errors:
-            for line in iter(process.stderr.readline, b''):
-                _sys.stdout.write(line)
-                f.write(line)
-        else:
-            return process.pid, process, port
+        return process
     except: #catch KeyboardInterrupt and whatever other calamity might befall our process
-        process.kill()
-
-# Pandas DataFrame to Datasette
-def pandas_to_datasette(df, name='this', table_name='table', db_root='./', if_exists='replace', show_errors=True, **kwargs):
-    # create the DB from csv
-    db_name = pandas_to_sqlite(df, name, table_name, db_root, if_exists) 
-    # now to launch it ...
-    port = kwargs.get('port',kwargs.get('p',None))
-    port = find_free_port() if not port else port
-    try:
-        del kwargs['port']
-    except:
-        pass
-    return start_datasette(db_name, show_errors=True, port=port, **kwargs)
-    
-# TODO: register Jupyter Magic
+        process.terminate()
