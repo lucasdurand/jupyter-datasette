d65c2a8aab8fb570d8400e62809cc8cd73ccd317
Durand <Lucas.Durand@tdsecurities.com>
2019-05-10 13:43:45 -0400

    Hide libs from user

--- jupyter_datasette/tools.py
+++ jupyter_datasette/tools.py
@@ -1,17 +1,18 @@
 # Some useful tools now that we have Datasette integrated with Jupyter
-import subprocess
-import sys
-import pandas as pd
-import socket
-from IPython.display import display, HTML
+import subprocess as _subprocess
+import sys as _sys
+import os as _os
+import socket as _socket
+from IPython.display import display as _display
+from IPython.display import HTML as _HTML
 
-import sqlite3
-from contextlib import closing
+import sqlite3 as _sqlite3
+from contextlib import closing as _closing
 
 def find_free_port():
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+    with _closing(_socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)) as s:
         s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
 # Let's keep this simple for now, unfortunately can't use csvs-to-sqlite to do more intricate setup (cli only)
@@ -23,9 +24,10 @@ def pandas_to_sqlite(df, name='this', table_name='table', db_root='./', if_exist
     '''
     table_name = df.table_name if 'table_name' in vars(df) else table_name
     name = df.name if 'name' in vars(df) else name
-    db_name = os.path.join(db_root,name)
+    db_name = _os.path.join(db_root,name)
+    db_name = f'{db_name}.db' if '.db' not in db_name else db_name
 
-    conn = sqlite3.connect(db_name)
+    conn = _sqlite3.connect(db_name)
     df.to_sql(table_name, conn, if_exists=if_exists)
     return db_name
 
@@ -34,20 +36,18 @@ def start_datasette(db_path, show_errors=True, **kwargs):
         Pass in cmdline flags as kwargs: port=8080, reload=True
     '''
     port = kwargs.get('port',kwargs.get('p',None))
-    port = find_free_port() if not port else port
-
-    host = socket.gethostbyname(socket.gethostname())
+    host = _socket.gethostbyname(_socket.gethostname())
 
     # keep the kwargs alive
     flags = [f'--{key}' if value is True else f'--{key} {value}' for key,value in kwargs.items()]
     flags = ' '.join(flags)
     cmd = f'datasette {db_path} {flags}'
-    display(HTML(f'''<b>Datasette Launching at <a href="{host}:{port}">{host}:{port}</a></b>'''))
+    _display(_HTML(f'''<b>Datasette Launching at <a target="_blank" href="{host}:{port}">{host}:{port}</a></b>'''))
     try:
-        process = subprocess.Popen(cmd.split(' '), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        process = _subprocess.Popen(cmd.split(' '), stderr=_subprocess.PIPE, stdout=_subprocess.PIPE)
         if show_errors:
             for line in iter(process.stderr.readline, b''):
-                sys.stdout.write(line)
+                _sys.stdout.write(line)
                 f.write(line)
         else:
             return process.pid, process, port
@@ -59,7 +59,12 @@ def pandas_to_datasette(df, name='this', table_name='table', db_root='./', if_ex
     # create the DB from csv
     db_name = pandas_to_sqlite(df, name, table_name, db_root, if_exists) 
     # now to launch it ...
-    return start_datasette(os.path.join(db_root,db_path), show_errors=True, **kwargs)
-
-
+    port = kwargs.get('port',kwargs.get('p',None))
+    port = find_free_port() if not port else port
+    try:
+        del kwargs['port']
+    except:
+        pass
+    return start_datasette(db_name, show_errors=True, port=port, **kwargs)
+    
 # TODO: register Jupyter Magic
